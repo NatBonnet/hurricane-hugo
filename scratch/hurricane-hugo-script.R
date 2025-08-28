33dd5e4c5bfdbbc49e7f603c0e7404e633fbb705
NatBonnet <nathaliehbonnet@gmail.com>
2025-08-28 11:11:20 -0700

    testing pages

--- scratch/hurricane-hugo-script.R
+++ scratch/hurricane-hugo-script.R
@@ -1,7 +1,7 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##                                                                            ~~
-##                       LOADING IN NECESSARY LIBRARIES                     ----
+##                       Test Script for working out my issues                     ----
 ##                                                                            ~~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -23,13 +23,18 @@ source("R/rolling_avg.R")
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##  ~ reading in the Bisley catchement CSVs  ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-prm <- read_csv(here::here("data", "RioMameyesPuenteRoto.csv"))
+prm <- read_csv(here::here("data", "RioMameyesPuenteRoto.csv"))|>
+  clean_names()
 
-q1 <- read_csv(here::here("data", "QuebradaCuenca1-Bisley.csv"))
+q1 <- read_csv(here::here("data", "QuebradaCuenca1-Bisley.csv"))|>
+  clean_names()
 
-q2 <- read_csv(here::here("data", "QuebradaCuenca2-Bisley.csv"))
+q2 <- read_csv(here::here("data", "QuebradaCuenca2-Bisley.csv")) |>
+  clean_names()
 
-q3 <- read_csv(here::here("data", "QuebradaCuenca3-Bisley.csv"))
+q3 <- read_csv(here::here("data", "QuebradaCuenca3-Bisley.csv"))|>
+  clean_names()|>
+  rename(no3 = )
 
 #............................test code...........................
 
@@ -37,11 +42,15 @@ q3 <- read_csv(here::here("data", "QuebradaCuenca3-Bisley.csv"))
 # create a subset with only sample date and K
 # apply the rolling average function to pull out the rolling average K concentration from 1988-1994
 q1c_trial <- q1 |>
-  select(Sample_Date, K) |>
-  filter(Sample_Date <= "1994-12-27", Sample_Date >= "1988-01-05") |>
-  mutate(K_mean = sapply(X = Sample_Date, 
+  select(sample_date, k) |>
+  filter(sample_date <= "1994-12-27", sample_date >= "1988-01-05") |>
+  mutate(k_mean = sapply(X = sample_date, 
                          FUN = roll_avg, 
-                         sample_date = Sample_Date,
-                         nutrient_conc = K, 
+                         sample_date = sample_date,
+                         nutrient_conc = k, 
                          win_size_wks = 10))
 
+
+#lets try doing this all across a csv- k, no3, mg, ca, nh4
+q1_roll_avg <- q1 |>
+  select(sample_id, sample_date, no3)
