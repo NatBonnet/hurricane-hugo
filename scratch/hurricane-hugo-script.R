17f4635954afa93cfe89aae69ac84a98d44580b6
NatBonnet <nathaliehbonnet@gmail.com>
2025-08-28 11:18:29 -0700

    started to apply mutate(roll_avg()) across columns

--- scratch/hurricane-hugo-script.R
+++ scratch/hurricane-hugo-script.R
@@ -24,17 +24,20 @@ source("R/rolling_avg.R")
 ##  ~ reading in the Bisley catchement CSVs  ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 prm <- read_csv(here::here("data", "RioMameyesPuenteRoto.csv"))|>
-  clean_names()
+  clean_names()|>
+  rename(no3 = no3_n, nh4 = nh4_n)
 
 q1 <- read_csv(here::here("data", "QuebradaCuenca1-Bisley.csv"))|>
-  clean_names()
+  clean_names()|>
+  rename(no3 = no3_n, nh4 = nh4_n)
 
 q2 <- read_csv(here::here("data", "QuebradaCuenca2-Bisley.csv")) |>
-  clean_names()
+  clean_names()|>
+  rename(no3 = no3_n, nh4 = nh4_n)
 
 q3 <- read_csv(here::here("data", "QuebradaCuenca3-Bisley.csv"))|>
   clean_names()|>
-  rename(no3 = )
+  rename(no3 = no3_n, nh4 = nh4_n)
 
 #............................test code...........................
 
@@ -53,4 +56,15 @@ q1c_trial <- q1 |>
 
 #lets try doing this all across a csv- k, no3, mg, ca, nh4
 q1_roll_avg <- q1 |>
-  select(sample_id, sample_date, no3)
+  select(sample_id, sample_date, k, no3, ca, mg, nh4)|>
+  filter(sample_date <= "1994-12-27", sample_date >= "1988-01-05")|>
+  mutate(k_mean = sapply(X = sample_date, 
+                         FUN = roll_avg, 
+                         sample_date = sample_date,
+                         nutrient_conc = k, 
+                         win_size_wks = 10))|>
+  mutate(no3_mean = sapply(X = sample_date, 
+                           FUN = roll_avg, 
+                           sample_date = sample_date,
+                           nutrient_conc = no3, 
+                           win_size_wks = 10))
